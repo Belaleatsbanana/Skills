44cc30b977bdb4a8617c8ca805b6d4f594ec658f
Fejgin, Roy <rfejgin@nvidia.com>
2026-02-11 22:45:56 -0800

    Misc updates
    Signed-off-by: Fejgin, Roy <rfejgin@nvidia.com>

--- eval_all_roy.sh
+++ eval_all_roy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
- NEMO_SKILLS_DISABLE_UNCOMMITTED_CHANGES_CHECK=1 \
+set -x
+STAGE="${1:-all}"
+NEMO_SKILLS_DISABLE_UNCOMMITTED_CHANGES_CHECK=1 \
 python -m nemo_skills.dataset.nv_tts.scripts.run_tts_eval \
   --config nemo_skills/dataset/nv_tts/scripts/config/tts-roy.yaml \
-  --stage all \
-  --expname test3
-
-# --stage all
+  --stage "$STAGE" \
+  --expname default_eval
