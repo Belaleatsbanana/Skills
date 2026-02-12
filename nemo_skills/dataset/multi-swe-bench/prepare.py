f033f254e59a290261b9c95e91d50ea7f155e652
Nikolai Ludwig <nliudvig@nvidia.com>
2026-02-12 18:01:11 +0400

    Support full subset
    Signed-off-by: Nikolai Ludwig <nliudvig@nvidia.com>

--- nemo_skills/dataset/multi-swe-bench/prepare.py
+++ nemo_skills/dataset/multi-swe-bench/prepare.py
@@ -14,9 +14,28 @@
 
 import argparse
 import json
+import subprocess
 from pathlib import Path
 
 from nemo_skills.dataset.utils import download_with_retries
+
+DATASET_NAME = {
+    "flash": "ByteDance-Seed/Multi-SWE-bench-flash",
+    "mini": "ByteDance-Seed/Multi-SWE-bench_mini",
+    "full": "ByteDance-Seed/Multi-SWE-bench",
+}
+
+
+def convert_language(language):
+    if language == "c++":
+        return "cpp"
+    elif language == "js":
+        return "javascript"
+    elif language == "ts":
+        return "typescript"
+    else:
+        return language
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -28,8 +47,9 @@ if __name__ == "__main__":
         "directory which you can reference here to avoid redownloading all the time. "
         "See nemo_skills/dataset/swe-bench/dump_images.py",
     )
-    # TODO: should flash be the default?
-    parser.add_argument("--subset", type=str, default="flash", help="Multi-SWE-bench subset to use")
+    parser.add_argument(
+        "--subset", type=str, default="full", help="Multi-SWE-bench subset to use. Must be one of: flash, mini, full."
+    )
     parser.add_argument(
         "--setup", type=str, default="default", help="Setup name (used as nemo-skills split parameter)."
     )
@@ -39,56 +59,75 @@ if __name__ == "__main__":
     container_formatter = args.container_formatter
 
     # Multi-SWE-bench has weird formatting in their jsonl files, so HF load_dataset doesn't work with it.
-    # Instead, we download the jsonl directly and format it to work with our swebench generation module.
+    # Instead, we download the jsonl files directly and format them to work with our swebench generation module.
 
-    output_file = Path(__file__).parent / f"{args.setup}.jsonl"
-    temp_file = Path(__file__).parent / f"{args.setup}.temp.jsonl"
+    temp_dir = Path(__file__).parent / ".temp"
+    temp_dir.unlink(missing_ok=True)
+    temp_dir.mkdir()
 
     if subset == "flash":
+        data_files = [temp_dir / "flash.jsonl"]
         download_with_retries(
             url="https://huggingface.co/datasets/ByteDance-Seed/Multi-SWE-bench-flash/resolve/main/multi_swe_bench_flash.jsonl",
-            output_file=temp_file,
+            output_file=data_files[0],
         )
     elif subset == "mini":
+        data_files = [temp_dir / "mini.jsonl"]
         download_with_retries(
             url="https://huggingface.co/datasets/ByteDance-Seed/Multi-SWE-bench_mini/resolve/main/multi_swe_bench_mini.jsonl",
-            output_file=temp_file,
+            output_file=data_files[0],
         )
+    elif subset == "full":
+        # Check if git-lfs is installed
+        try:
+            subprocess.run(
+                ["git", "lfs", "--version"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+            )
+        except subprocess.CalledProcessError:
+            print("Git LFS is not installed. Please install it to prepare the full Multi-SWE-bench dataset.")
+            exit(1)
+
+        # Clone the Multi-SWE-bench dataset
+        subprocess.run(
+            ["git", "clone", "https://huggingface.co/datasets/ByteDance-Seed/Multi-SWE-bench", str(temp_dir)],
+            check=True,
+        )
+
+        # Get a list of all jsonl files
+        data_files = sorted(list(temp_dir.glob("*/*.jsonl")))
     else:
-        # TODO: support full split
-        raise ValueError(f"Subset {subset} is not supported. Must be either 'flash' or 'mini'.")
+        raise ValueError(f"Subset '{subset}' is not supported. Must be one of: flash, mini, full.")
 
-    with open(temp_file, "r") as fin, open(output_file, "w") as fout:
-        for i, line in enumerate(fin):
-            input_row = json.loads(line)
+    output_file = Path(__file__).parent / f"{args.setup}.jsonl"
 
-            # Multi-SWE-bench docker image format: mswebench/facebook_m_zstd:pr-3942
-            docker_image = f"mswebench/{input_row['org']}_m_{input_row['repo']}:pr-{input_row['number']}".lower()
-            if container_formatter.endswith(".sif"):
-                docker_image = docker_image.replace("/", "_").replace(":", "_")
+    for data_file in data_files:
+        with open(data_file, "r") as fin, open(output_file, "w") as fout:
+            for i, line in enumerate(fin):
+                input_row = json.loads(line)
 
-            # Convert instance from Multi-SWE-bench format to SWE-bench Verified format:
-            # https://github.com/OpenHands/OpenHands/blob/main/evaluation/benchmarks/multi_swe_bench/scripts/data/data_change.py#L30
-            issue = input_row["resolved_issues"][0]
-            output_row = {
-                "instance_id": input_row["instance_id"],
-                "repo": input_row["org"] + "/" + input_row["repo"],
-                "language": input_row["language"],
-                "base_commit": input_row["base"]["sha"],
-                "problem_statement": issue["title"] + "\n" + issue["body"],
-                "container_formatter": container_formatter.format(docker_image=docker_image),
-                "container_id": i,
-                "dataset_name": (
-                    "ByteDance-Seed/Multi-SWE-bench-flash"
-                    if subset == "flash"
-                    else "ByteDance-Seed/Multi-SWE-bench_mini"
-                ),
-                "split": "train",
-                # We save the original row because the evaluation harness will need it.
-                # We save it as a string to prevent HF load_dataset errors.
-                "original_row": json.dumps(input_row),
-            }
+                # Multi-SWE-bench docker image format: mswebench/facebook_m_zstd:pr-3942
+                docker_image = f"mswebench/{input_row['org']}_m_{input_row['repo']}:pr-{input_row['number']}".lower()
+                if container_formatter.endswith(".sif"):
+                    docker_image = docker_image.replace("/", "_").replace(":", "_")
 
-            print(json.dumps(output_row), file=fout)
+                # Convert instance from Multi-SWE-bench format to SWE-bench Verified format:
+                # https://github.com/OpenHands/OpenHands/blob/main/evaluation/benchmarks/multi_swe_bench/scripts/data/data_change.py#L30
+                issue = input_row["resolved_issues"][0]
+                output_row = {
+                    "instance_id": input_row["instance_id"],
+                    "repo": input_row["org"] + "/" + input_row["repo"],
+                    "language": convert_language(data_file.parent.name if subset == "full" else input_row["language"]),
+                    "base_commit": input_row["base"]["sha"],
+                    "problem_statement": issue["title"] + "\n" + issue["body"],
+                    "container_formatter": container_formatter.format(docker_image=docker_image),
+                    "container_id": i,
+                    "dataset_name": DATASET_NAME[subset],
+                    "split": "train",
+                    # We save the original row because the evaluation harness will need it.
+                    # We save it as a string to prevent HF load_dataset errors.
+                    "original_row": json.dumps(input_row),
+                }
 
-    temp_file.unlink()
+                print(json.dumps(output_row), file=fout)
+
+    temp_dir.unlink()
